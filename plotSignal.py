138de0fc0daefa512315ee9b34400cf77ba29b2e
huang <hhk0@outlook.com>
2019-10-16 16:31:17 +0800

    update

--- plotSignal.py
+++ plotSignal.py
@@ -31,6 +31,17 @@ class TsTickData(object):
                 update t set ['time']=datetimetostr(['time']) end;
                 return t;
         '''.format(ticker, date)
+        # tsl_sql =   '''
+        #             begT:= StrToDate('{{start_date}}');
+        #             endT:= StrToDate('{{end_date}}');
+        #             setsysparam(pn_cycle(),cy_1s());
+        #             setsysparam(pn_rate(),0);
+        #             setsysparam(pn_RateDay(),rd_lastday);
+        #             r:= select datetimetostr(["date"]) as "time", ["price"],
+        #                 from markettable datekey begT to endT of "{{code}}" end;
+        #             return r;'''
+
+
         fail, data, _ = ts.RemoteExecute(ts_sql, {})
         df = pd.DataFrame(data)
         df = df[[b"time", b"price"]]
@@ -39,12 +50,24 @@ class TsTickData(object):
         df["time"] = df["time"].apply(lambda s: dt.datetime.strptime(s, "%Y-%m-%d %H:%M:%S"))
         df.sort_values(by="time", inplace=True)
         start_time = dt.datetime.strptime(date + "093000", "%Y%m%d%H%M%S")
-        end_time = dt.datetime.strptime(date + "113000", "%Y%m%d%H%M%S")
+        end_time = dt.datetime.strptime(date + "150000", "%Y%m%d%H%M%S")
         df = df[(df["time"] >= start_time) & (df["time"] <= end_time)]
-        df["time"] = df["time"].apply(lambda t: t.time())
-        # df.index = pd.to_datetime(df["time"])
-        # df.resample("1S")
-        # print(df)
+        df["time"] = df["time"].apply(lambda t: str(t.time()))
+        df.index = df["time"]
+        N = df.shape[0]
+        for i in range(N):
+            if i < N - 1:
+                price = df.iloc[i, 1]
+                time1 = dt.datetime.strptime(df.iloc[i, 0], "%H:%M:%S")
+                time2 = dt.datetime.strptime(df.iloc[i + 1, 0], "%H:%M:%S")
+                t = time1 + dt.timedelta(0, 1)
+                while t < time2:
+                    df = df.append(pd.DataFrame([[str(t.time()), price], ], columns=["time", "price"]))
+                    t += dt.timedelta(0, 1)
+        df.sort_values(by="time", inplace=True)
+        df = df[(df["time"] <= "11:30:00") | (df["time"] >= "13:00:00")]
+        df["time_offset"] = list(range(df.shape[0]))
+        # df.to_csv("debug.csv")
         return df
 
 if __name__ == "__main__":
@@ -55,35 +78,36 @@ if __name__ == "__main__":
     df["name"] = df["代码/名称"].apply(lambda s: s[7:])
     df.columns = ["time", "ticker/name", "direction", "quantity", "price", "status", "ticker", "name"]
     df = df[["time", "ticker", "name", "price", "quantity", "direction", "status"]]
-    df["time"] = df["time"].apply(lambda s: dt.datetime.strptime(s, "%Y-%m-%d %H:%M:%S").time())
+    df["time"] = df["time"].apply(lambda s: str(dt.datetime.strptime(s, "%Y-%m-%d %H:%M:%S").time()))
     ticker_set = set(sorted(df["ticker"].tolist()))
-    print(len(ticker_set))
+    i = 0
     for ticker in ticker_set:
-        with TsTickData() as obj:
-            data = obj.ticks(ticker=ticker, date=date)
-        plt.figure(figsize=(30, 15))
-        plt.plot(data["time"], data["price"], color="gray", alpha=0.5)
-        sub_df = df[df["ticker"] == ticker]
-        sub_df["pct"] = round(sub_df["quantity"] / sub_df["quantity"].sum() * 100).astype(int)
-        for key, record in sub_df.iterrows():
-            # print(str(record["time"]))
-            # print(dt.datetime.strftime(record["time"], "%H:%M:%S"))
-            if str(record["time"])> "11:30:00":
-                continue
-            if record["direction"] == "买入" and record["status"] == "已成":
-                marker = 'or'
-            elif record["direction"] == "卖出" and record["status"] == "已成":
-                marker = 'sg'
-            elif record["direction"] == "买入" and record["status"] == "已撤":
-                marker = '*r'
-            elif record["direction"] == "卖出" and record["status"] == "已撤":
-                marker = 'xg'
-            plt.plot([record["time"],], [record["price"],], marker, markersize=10)
-            plt.text(record["time"], record["price"] + 0.01, str(record["pct"]) +'%')
-        # plt.xlim(df["time"].tolist()[0], df["time"].tolist()[-1])
-        # print(sub_df["time"].tolist())
-        # print(sub_df["price"].tolist())
-        # plt.plot(sub_df["time"], sub_df["price"], "*")
-        plt.title(ticker + " | " + date, fontsize=25)
-        plt.savefig("pictures_only_morning/" + ticker + '_' + date + '.png')
-        plt.close()
+        if i < 57:
+            pass
+        else:
+            with TsTickData() as obj:
+                data = obj.ticks(ticker=ticker, date=date)
+            plt.figure(figsize=(20, 10))
+            plt.plot(data["time_offset"], data["price"], color="gray", alpha=0.5)
+            sub_df = df[df["ticker"] == ticker]
+            sub_df["pct"] = round(sub_df["quantity"] / sub_df["quantity"].sum() * 100).astype(int)
+            for key, record in sub_df.iterrows():
+                if record["direction"] == "买入" and record["status"] == "已成":
+                    marker = 'or'
+                elif record["direction"] == "卖出" and record["status"] == "已成":
+                    marker = 'sg'
+                elif record["direction"] == "买入" and record["status"] == "已撤":
+                    marker = '*r'
+                elif record["direction"] == "卖出" and record["status"] == "已撤":
+                    marker = 'xg'
+                time_offset = data[data["time"] == record["time"]]["time_offset"].squeeze()
+                plt.plot([time_offset,], [record["price"],], marker, markersize=6)
+                plt.text(time_offset, record["price"] + 0.01, str(record["pct"]) +'%')
+            xticks = list(range(14401))[::1800]
+            xticklabels = ["9:30", "10:00", "10:30", "11:00", "11:30/13:00", "13:30", "14:00", "14；30", "15:00"]
+            plt.xticks(xticks, xticklabels)
+            plt.title(ticker + " | " + date, fontsize=25)
+            plt.savefig("pictures/" + ticker + '_' + date + '.png')
+            plt.close()
+        i += 1
+        print("Process " + str(i) + " stocks")
